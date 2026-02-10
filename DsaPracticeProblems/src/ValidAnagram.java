0912265e650529457a24979bec198d5438b2ab63
Karthikeyan <karthialdo27.careers@gmail.com>
2026-02-10 23:56:20 +0530

    ValidAnagram - String basics problem Added.

---
+++ DsaPracticeProblems/src/ValidAnagram.java
@@ -0,0 +1,10 @@
+public class ValidAnagram {
+        String s = "car";
+        String t = "rat";
+    static public boolean validAnagram() {
+
+        if(s.length() != t.length())
+            return false;
+        return false;
+    }
+}
